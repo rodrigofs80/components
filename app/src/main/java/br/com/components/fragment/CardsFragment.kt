dce88de177a236132a483cfa4ff8a6bb86f37c6d
Rodrigo Ferreira <rodrigofs80@gmail.com>
2020-06-08 12:47:51 -0300

    Transition Detail

--- app/src/main/java/br/com/components/fragment/CardsFragment.kt
+++ app/src/main/java/br/com/components/fragment/CardsFragment.kt
@@ -1,15 +1,23 @@
 package br.com.components.fragment
 
+import android.net.Uri
 import android.os.Bundle
+import android.transition.Fade
+import android.transition.TransitionSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.components.R
+import br.com.components.activity.MainActivity
 import kotlinx.android.synthetic.main.fragment_cards.*
+import kotlinx.android.synthetic.main.fragment_cards.view.*
 
 class CardsFragment : Fragment(), OnBackPress {
+
+    private val MOVE_DEFAULT_TIME: Long = 1000
+    private val FADE_DEFAULT_TIME: Long = 300
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_cards, container, false)
@@ -34,18 +42,56 @@ class CardsFragment : Fragment(), OnBackPress {
                 ll_detail2.visibility = View.GONE
             }
         }
+
+        val uri: Uri = Uri.parse("android.resource://br.com.components/drawable/geralt")
         card3.setOnClickListener {
-            activity?.let {
-                it.supportFragmentManager.beginTransaction()
-                    .addSharedElement(image3,"myImage")
-                    .addSharedElement(titulo3,"myTitulo")
-                    .addSharedElement(sub_titulo3,"mySubTitulo")
-                    .addSharedElement(descricao3,"myDescricao")
-                    .addSharedElement(detalhe3,"myDetalhe")
-                    .addSharedElement(profissao3,"myProfissao")
-                    .replace(R.id.container_root, DetailCardFragment.newInstance()).addToBackStack("frag-tag").commit() }
+            performTransition(uri)
         }
-        fab_back.setOnClickListener {onBackPress()}
+        fab_back.setOnClickListener {
+            activity?.let {
+                it.supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container_root, MainFragment.newInstance())
+                        .addToBackStack(MainActivity.FRAGMENT_TAG).commit()}
+        }
+    }
+
+    private fun performTransition(uri: Uri) {
+         val previousFragment = activity?.let{
+             it.supportFragmentManager.findFragmentById(R.id.container_root)
+         }
+
+        val nextFragment = DetailCardFragment(uri)
+        val fragmentTransaction = activity?.let { it.supportFragmentManager.beginTransaction() }
+
+        // 1. Exit for Previous Fragment
+        val exitFade = Fade()
+        exitFade.duration = FADE_DEFAULT_TIME
+        previousFragment!!.exitTransition = exitFade
+
+        // 2. Shared Elements Transition
+        val enterTransitionSet = TransitionSet()
+        enterTransitionSet.addTransition(
+            android.transition.TransitionInflater.from(activity).inflateTransition(android.R.transition.move)
+        )
+        enterTransitionSet.duration = MOVE_DEFAULT_TIME
+        enterTransitionSet.startDelay = FADE_DEFAULT_TIME
+        nextFragment.setSharedElementEnterTransition(enterTransitionSet)
+
+        // 3. Enter Transition for New Fragment
+        val enterFade = Fade()
+        enterFade.startDelay = MOVE_DEFAULT_TIME + FADE_DEFAULT_TIME
+        enterFade.duration = FADE_DEFAULT_TIME
+        nextFragment.setEnterTransition(enterFade)
+
+        val logo = card3.image3
+        fragmentTransaction?.let{
+            it.apply {
+                this.addSharedElement(logo, logo.transitionName)
+            }
+            .replace(R.id.container_root, nextFragment)
+            .commitAllowingStateLoss()
+        }
     }
 
     private fun showToast(msg: String){
@@ -53,6 +99,10 @@ class CardsFragment : Fragment(), OnBackPress {
     }
 
     override fun onBackPress() {
-        activity?.onBackPressed()
+        activity?.let {
+            it.supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container_root, CardsFragment())
+                    .addToBackStack(MainActivity.FRAGMENT_TAG).commit() }
     }
 }
--- app/src/main/java/br/com/components/fragment/DetailCardFragment.kt
+++ app/src/main/java/br/com/components/fragment/DetailCardFragment.kt
@@ -1,33 +1,43 @@
 package br.com.components.fragment
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import br.com.components.R
-import kotlinx.android.synthetic.main.fragment_cards.*
+import br.com.components.activity.MainActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fab_back.fab_back
+import kotlinx.android.synthetic.main.fragment_detail_card.view.*
 
-class DetailCardFragment : Fragment(), OnBackPress {
+class DetailCardFragment() : Fragment() {
 
-    companion object{
-        fun newInstance() = DetailCardFragment()
+    var uri: Uri? = null
+
+    constructor(uri: Uri): this() {
+        this.uri = uri
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail_card, container, false)
+        var view = inflater.inflate(R.layout.fragment_detail_card, container, false)
+        view.setTransitionName(view.transitionName)
+        Glide.with(this)
+            .load(uri)
+            .into(view.image as ImageView)
+        return  view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        initItems()
-    }
-
-    private fun initItems(){
-        fab_back.setOnClickListener {onBackPress()}
-    }
-
-    override fun onBackPress() {
-        activity?.onBackPressed()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab_back.setOnClickListener {
+            activity?.let {
+                it.supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container_root, CardsFragment())
+                        .addToBackStack(MainActivity.FRAGMENT_TAG).commit() }
+        }
     }
 }
